0924281b1018fdc75631499336048f01c7d657f1
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-30 09:02:59 +0100

    code javascript

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,8 @@ const sr= ScrollReveal({
 });
 sr.reveal(".footer-text, .home, .services, .chefs, .contact", {interval:200})
 
+
+// partie reserve au typewriter
 const title=document.querySelector("h1")
 const app = new Typewriter(title, {
     loop:true,
@@ -18,3 +20,12 @@ const app = new Typewriter(title, {
 .deleteAll()
 .pauseFor(2000)
 .start();
+
+
+// partie reserves au menu burger
+
+const menu=document.querySelector(".bars");
+const navigation=document.querySelector(".navigation");
+menu.addEventListener("click", ()=>{
+    navigation.classList.toggle("active2")
+})
